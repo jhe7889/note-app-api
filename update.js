e8ea8fe18b78a1c53d9bbc6620238571a7831d7a
jhe7889 <jinghe7889@hotmail.com>
2017-12-30 20:57:10 -0500

    add files

---
+++ update.js
@@ -0,0 +1,30 @@
+import * as dynamoDbLib from "./libs/dynamodb-libs";
+import { success, failure } from "./libs/response-lib";
+export async function main(event, context, callback) 
+{ 
+
+const data = JSON.parse(event.body);
+const params = {
+     TableName: "notes",
+    
+Key: {
+       userId: event.requestContext.identity.cognitoIdentityId,
+       noteId: event.pathParameters.id
+     },
+     // 'UpdateExpression' defines the attributes to be updated
+     // 'ExpressionAttributeValues' defines the value in the update
+ 
+     UpdateExpression: "SET content = :content, attachment =:attachment",
+      ExpressionAttributeValues: {
+      ":attachment": data.attachment ? data.attachment : null, ":content": data.content ? data.content : null
+      },
+        ReturnValues: "ALL_NEW"
+      };
+    
+ 
+try {
+const result = await dynamoDbLib.call("update", params); callback(null, success({ status: true }));
+} catch (e) {
+callback(null, failure({ status: false }));
+}
+}
